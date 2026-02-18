d09f570b083907bb993df8ab6c6db995d0ad5237
MustyX23 <mustyx900@gmail.com>
2026-02-18 22:16:44 +0200

    add comment

--- HouseRentingSystem.Services/Agents/Models/AgentServiceModel.cs
+++ HouseRentingSystem.Services/Agents/Models/AgentServiceModel.cs
@@ -2,6 +2,7 @@
 {
     public class AgentServiceModel
     {
+        // comment
         public string FullName { get; set; }
         public string Email { get; init; }
         public string PhoneNumber { get; init; }
